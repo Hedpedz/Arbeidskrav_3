41af2712ce83da7c463a6b03113dd009280d9550
Hedpedz <heddaon@hiof.no>
2025-04-16 15:08:37 +0200

    Refactor Sanity client configuration by removing unused image URL builder import and cleaning up comments

--- react-app/src/sanity/client.js
+++ react-app/src/sanity/client.js
@@ -1,11 +1,10 @@
 import sanityClient from '@sanity/client';
 import client from '@sanity/client';
-import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: 'x9fw47rf', // Finn dette i Sanity Studio
+  projectId: 'x9fw47rf', 
   dataset: 'production',
-  apiVersion: '2023-01-01', // Bruk en aktuell API-versjon
-  useCdn: false, // For raskere lesing, men uten token for skriving
+  apiVersion: '2023-01-01', 
+  useCdn: false, 
 });
 
