c5b6961675b5ef6deeb7bdbd83d29e5f528d7970
Hedpedz <heddaon@hiof.no>
2025-04-20 17:07:02 +0200

    Update Sanity client configuration and add .hintrc file

--- react-app/src/sanity/client.js
+++ react-app/src/sanity/client.js
@@ -1,10 +1,9 @@
 import sanityClient from '@sanity/client';
-import client from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: 'x9fw47rf', 
+  projectId: 'c5g69vfr', 
   dataset: 'production',
-  apiVersion: '2023-01-01', 
+  apiVersion: 'v2025-07-04', 
   useCdn: false, 
 });
 
