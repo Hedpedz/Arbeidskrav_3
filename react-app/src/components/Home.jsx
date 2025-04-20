4d8d71070343e90b2bca205a85281d7c232fd29d
Markus Olsen <markusol@hiof.no>
2025-04-20 02:07:45 +0200

    Fixed bancend functionality- render data from sanity- database to homepage, including the component UserCard

--- react-app/src/components/Home.jsx
+++ react-app/src/components/Home.jsx
@@ -1,18 +1,37 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { client } from "../sanity/client"; 
 import UserCard from "./UserCard";
 
 export default function Home() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchMembers = async () => {
+      const query = `*[_type == "member"]{
+        name,
+        firstName,
+        "slug": slug.current,
+        email,
+        "profileImage": profileImage.asset->url
+      }`;
+
+      const data = await client.fetch(query);
+      setUsers(data);
+    };
+
+    fetchMembers();
+  }, []);
+
   return (
     <>
-    <h1>Gruppemedlemmer</h1>
-    <div className="userCardContainer">
-        <UserCard></UserCard>
-        <UserCard></UserCard>
-        <UserCard></UserCard>
-    </div>
+      <h1>Gruppemedlemmer</h1>
+      <div className="userCardContainer">
+        {users.map((user) => (
+          <UserCard key={user.slug} user={user} />
+        ))}
+      </div>
 
-    <h1>Arbeidslogg</h1>
+      <h1>Arbeidslogg</h1>
     </>
-
   );
 }
